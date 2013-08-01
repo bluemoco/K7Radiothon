d227c05c02ef025858f21562a0761ff35db15466
bluemoco <louis@bluemoco.com>
2013-08-01 11:24:19 +0100

    Tooltipas and Refinements
    Tooltips has been added to the public page.

--- public.php
+++ public.php
@@ -4,11 +4,73 @@ include_once("config/connect.php");
 //2013-05-23 10:30:00 Time format in mysql
 $currentDateTime = date('Y-m-d H:i:s');
 
-//$getClosestToCloseItemQuery = "SELECT ai.*, IFNULL((SELECT MAX(AMOUNT) FROM bids b WHERE b.AucID=ai.AucID),0) as highest_bid,(SELECT COUNT(*) FROM bids b WHERE b.AucID=ai.AucID) as bidcount FROM `auctionitems` ai WHERE `CloseDateTime`>'" . $currentDateTime . "' ORDER BY `CloseDateTime` ASC LIMIT 10";
 
-$getClosestToCloseItemQuery = "SELECT ai.*, IFNULL((SELECT MAX(AMOUNT) FROM bids b WHERE b.AucID=ai.AucID),0) as highest_bid,(SELECT COUNT(*) FROM bids b WHERE b.AucID=ai.AucID) as bidcount FROM `auctionitems` ai WHERE `CloseDateTime`>'" . $currentDateTime . "'";
-//echo $getClosestToCloseItemQuery;
-$getClosestToCloseItemQueryResult = mysql_query($getClosestToCloseItemQuery);
+
+/*
+ * Initialize variables with default value
+ */
+$town = 0;
+$timeleft = "";
+$priceSort = "";
+$bidSort = "";
+$itemName = "";
+$qurey_string = "";
+if(isset($_GET['town'])){
+    $town = $_GET['town'];
+    $timeleft = $_GET['timeleft'];
+    $priceSort = $_GET['priceSort'];
+    $bidSort = $_GET['bidSort'];
+    $itemName = $_GET['itemName'];
+    
+    $qurey_string = "?town=".$town."&timeleft=".$timeleft."&priceSort=".$priceSort."&bidSort=".$bidSort."&itemName=".$itemName;
+}
+
+
+/*
+ * Search Conditions
+ */
+$condition = "";
+if ($town != 0) {//Check for town by its id
+    $condition.= " AND TownID='" . $town . "'";
+}
+if($itemName!=""){
+    $condition.=" AND ItemName REGEXP '".$itemName."'";
+}
+
+$orderByArray=array();
+if($timeleft!=""){
+    $orderByArray[0]="`CloseDateTime` ".$timeleft;
+}
+if($priceSort!=""){
+    $orderByArray[1]="`startprice` ".$priceSort;
+}
+if($bidSort!=""){
+    $orderByArray[2]="`bidcount` ".$bidSort;
+}
+
+if(!empty($orderByArray)){
+    $orderBy="ORDER BY ".  implode(",", $orderByArray);
+}
+
+
+
+include('lib/pager.class.php');
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 1;
+}
+
+//$getClosestToCloseItemQuery = "SELECT ai.*, IFNULL((SELECT MAX(AMOUNT) FROM bids b WHERE b.AucID=ai.AucID),0) as highest_bid,(SELECT COUNT(*) FROM bids b WHERE b.AucID=ai.AucID) as bidcount FROM `auctionitems` ai WHERE `CloseDateTime`>'" . $currentDateTime . "'";
+$getClosestToCloseItemQuery = "SELECT ai.*, IFNULL((SELECT MAX(AMOUNT) FROM bids b WHERE b.AucID=ai.AucID),0) as highest_bid,(SELECT COUNT(*) FROM bids b WHERE b.AucID=ai.AucID) as bidcount FROM `auctionitems` ai WHERE `CloseDateTime`>'" . $currentDateTime . "' " . $condition ." ".$orderBy;
+//$getClosestToCloseItemQuery .= " ORDER BY ai.AucID DESC";
+
+$o__pager = new pager('auctionitems', $page, 30, array(), $getClosestToCloseItemQuery);
+$getClosestToCloseItemQueryResult = $o__pager->getQuery();
+
+
+//$getClosestToCloseItemQuery = "SELECT ai.*, IFNULL((SELECT MAX(AMOUNT) FROM bids b WHERE b.AucID=ai.AucID),0) as highest_bid,(SELECT COUNT(*) FROM bids b WHERE b.AucID=ai.AucID) as bidcount FROM `auctionitems` ai WHERE `CloseDateTime`>'" . $currentDateTime . "' LIMIT 0,30";
+//$getClosestToCloseItemQueryResult = mysql_query($getClosestToCloseItemQuery);
 $count = 0;
 $latestItemNearToClose = '<div class="row-fluid bottom-border" style="margin-bottom: 20px; padding-bottom:20px;">';
 while ($item = mysql_fetch_array($getClosestToCloseItemQueryResult)) {
@@ -17,14 +79,16 @@ while ($item = mysql_fetch_array($getClosestToCloseItemQueryResult)) {
     }
 
     $highest_bid = ($item['highest_bid'] != 0) ? "N$" . $item['highest_bid'] : "No bid placed";
-    $imageName=($item['Photo']=='')?'big_logo.png':$item['Photo'];
+    $imageName = ($item['Photo'] == '') ? 'VerticalLogo.png' : $item['Photo'];
     $imagePath = "uploads/" . $imageName;
     $tmpImage = "images/biltong.jpg";
-
+    $titleItem=$item['ItemName'];
+    $shortTitleItem=substr($item['ItemName'],0,35);
+    $additionalDots=($titleItem==$shortTitleItem)?'':'..';
     $latestItemNearToClose.='<div class="span4">
                         <div class="row-fluid">
                             <div class="span10">
-                                <div class="text-center"> <h2 class="text-info">' . $item['ItemName'] . '</h2> 
+                                <div class="text-center"> <h2 class="text-info item_title" title="'.$titleItem.'">' . $shortTitleItem .$additionalDots.'</h2> 
                                     <h5 class="text-info1">' . $item['AucID'] . '</h5>
                                     <img src="' . $imagePath . '" class="thumbnail">
                                     <h5>Time Left</h5>  <h3 id="countdown' . $count . '"></h3>
@@ -46,12 +110,17 @@ while ($item = mysql_fetch_array($getClosestToCloseItemQueryResult)) {
 }
 $latestItemNearToClose.='</div>';
 
+/*
+ * Town combo
+ */
 $townComboQuery = "SELECT * FROM  `town` ";
 $townComboQueryResult = mysql_query($townComboQuery);
 $townCombo = '<select class=" inpt" name="town">';
 $townCombo.='<option value="0">Select City</option>';
-while ($town = mysql_fetch_array($townComboQueryResult)) {
-    $townCombo.='<option value="' . $town['TownID'] . '">' . $town['TownName'] . '</option>';
+while ($townRow = mysql_fetch_array($townComboQueryResult)) {
+    $selected="";
+    if($town==$townRow['TownID']){$selected="selected";}
+    $townCombo.='<option value="' . $townRow['TownID'] . '" '.$selected.'>' . $townRow['TownName'] . '</option>';
 }
 $townCombo.='</select>';
 ?>
@@ -80,6 +149,8 @@ $townCombo.='</select>';
         <![endif]-->
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700' rel='stylesheet' type='text/css'>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+        <script src="js/tooltips.js"></script>
+        <link href='css/tooltips.css' rel='stylesheet' type='text/css'>
         <script type="text/javascript" src="js/jquery.countdown.min.js" ></script>
         <style type="text/css">
             #countdown { width: 240px; height: 45px; }
@@ -99,32 +170,10 @@ $townCombo.='</select>';
                 display: block;
             }
             .login-bg{display: none;}
+            .item_title{min-height: 60px;}
         </style>
         <script type="text/javascript">
-            function search(){
-                $("#auction_items").html("<img src='images/loading.gif' style='margin-left:210px;'/>");
-                $.ajax({
-                    type: "POST",
-                    url: "search_auction_items.php",
-                    data: $("#searchForm").serialize(),
-                    success: function(response)
-                    {				
-                        $("#auction_items").html(response); 
-                        $(".toggle").click(function() 
-                        {  
-                            // hides children divs if shown, shows if hidden 
-                            $(this).next().toggle("medium",function() {
-                                // Animation complete.
-                                if($(this).prev().html()=='View Details'){
-                                    $(this).prev().html('Hide Details');
-                                } else {
-                                    $(this).prev().html('View Details');
-                                }
-                            }); 
-                        });
-                    }
-                });
-            }
+            
             $(document).ready(function(){
                 $("input[type='reset']").on("click", function(event){
 
@@ -134,7 +183,7 @@ $townCombo.='</select>';
                     $(this).closest('form').get(0).reset();
                     // resets the form before continuing the function
                     
-                    search();
+                    $(this).closest('form').get(0).submit();
 
                 });
             });
@@ -156,7 +205,7 @@ $townCombo.='</select>';
             <div class="matter">
                 <div class="fheading">
                     <h1>ITEMS OPEN FOR BIDDING</h1></div>
-
+                
                 <div class="row-fluid" style="margin-bottom: 20px;">
                     <div class="span9">
                         <div class="row-fluid">
@@ -176,16 +225,16 @@ $townCombo.='</select>';
                                     <div class="controls1">
                                         <select class=" inpd1" name="timeleft">
                                             <option value="">Select Time Left</option>
-                                            <option value="asc">Least time left</option>
-                                            <option value="desc">Most Time Left</option>
+                                            <option value="asc" <? if($timeleft=="asc"){echo "selected";} ?>>Least time left</option>
+                                            <option value="desc"<? if($timeleft=="desc"){echo "selected";} ?>>Most Time Left</option>
                                         </select> 
                                     </div>
                                     <label class="control-label3" for="price" style="width:70px;">Item Price: </label>
                                     <div class="controls1">
                                         <select class=" inpd1" name="priceSort">
                                             <option value="">Select Price Sort</option>
-                                            <option value="asc">Lowest To Highest</option>
-                                            <option value="desc">Highest to lowest</option>
+                                            <option value="asc" <? if($priceSort=="asc"){echo "selected";} ?>>Lowest To Highest</option>
+                                            <option value="desc" <? if($priceSort=="desc"){echo "selected";} ?>>Highest to lowest</option>
                                         </select> 
                                     </div>
                                 </div>
@@ -196,8 +245,8 @@ $townCombo.='</select>';
                                     <div class="controls">
                                         <select class=" inpt" name="bidSort">
                                             <option value="">Select bid activity</option>
-                                            <option value="asc">Least bids</option>
-                                            <option value="desc">Most Bids</option> 
+                                            <option value="asc" <? if($bidSort=="asc"){echo "selected";} ?>>Least bids</option>
+                                            <option value="desc" <? if($bidSort=="desc"){echo "selected";} ?>>Most Bids</option> 
                                         </select> 
                                     </div>
                                 </div>
@@ -205,14 +254,14 @@ $townCombo.='</select>';
                                 <div class="control-group">
                                     <label class="control-label" for="itemname">Enter Item Name:</label>
                                     <div class="controls">
-                                        <input class="inpt1" type="text" id="itemName" name="itemName" > 
+                                        <input class="inpt1" type="text" id="itemName" name="itemName" value="<?=$itemName; ?>" > 
                                     </div>
                                 </div>
 
                                 <div class="control-group">
 
                                     <div class="controls">
-                                        <input type="button" class="btn btn-success" onclick="search();" value="Search" />
+                                        <input type="submit" class="btn btn-success" onclick="search();" value="Search" />
                                         <input type="reset" class="btn btn-info" value="Reset" />  
                                     </div>
                                 </div>
@@ -225,122 +274,28 @@ $townCombo.='</select>';
 
 
                 <div id="auction_items">
-                    <?= $latestItemNearToClose; ?>
+                    <?=$latestItemNearToClose; ?>
+                </div>
+
+                <div class="pagination pagination-right">
+                    <ul>
+                        <?php echo $o__pager->getPagerString('public_new.php' . $qurey_string); ?>
+                    </ul>
                 </div>
                 <div id="countdown" class="countdown"></div>
 
-                <!--                <div class="row-fluid bottom-border" style="margin-bottom: 20px; padding-bottom:20px;">
-                                    <div class="span4">
-                                        <div class="row-fluid">
-                                            <div class="span10">
-                                                <div class="text-center"> <h2 class="text-info">Kidu Bilton</h2> 
-                                                    <h5 class="text-info1">02154</h5>
-                                                    <img src="images/biltong.jpg" class="thumbnail">
-                                                    <h5>Time Left</h5>  <h2>24 : 12 : 10</h2>
-                                                    <h5 class="text-info">Highest Bid</h5>
-                                                    <h2 class="text-info">$33</h2>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                
-                                    <div class="span4">
-                                        <div class="row-fluid">
-                                            <div class="span10">
-                                                <div class="text-center"> <h2 class="text-info">Kidu Bilton</h2> 
-                                                    <h5 class="text-info1">02154</h5>
-                                                    <img src="images/biltong.jpg" class="thumbnail">
-                                                    <h5>Time Left</h5>  <h2>24 : 12 : 10</h2>
-                                                    <h5 class="text-info">Highest Bid</h5>
-                                                    <h2 class="text-info">$33</h2>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                
-                                    <div class="span4">
-                                        <div class="row-fluid">
-                                            <div class="span10">
-                                                <div class="text-center"> <h2 class="text-info">Kidu Bilton</h2> 
-                                                    <h5 class="text-info1">02154</h5>
-                                                    <img src="images/biltong.jpg" class="thumbnail">
-                                                    <h5>Time Left</h5>  <h2>24 : 12 : 10</h2>
-                                                    <h5 class="text-info">Highest Bid</h5>
-                                                    <h2 class="text-info">$33</h2>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row-fluid" style="margin-bottom: 20px;">
-                                    <div class="span4">
-                                        <div class="row-fluid">
-                                            <div class="span10">
-                                                <div class="text-center"> <h2 class="text-info">Kidu Bilton</h2> 
-                                                    <h5 class="text-info1">02154</h5>
-                                                    <img src="images/biltong.jpg" class="thumbnail">
-                                                    <h5>Time Left</h5>  <h2>24 : 12 : 10</h2>
-                                                    <h5 class="text-info">Highest Bid</h5>
-                                                    <h2 class="text-info">$33</h2>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="span4">
-                                        <div class="row-fluid">
-                                            <div class="span10">
-                                                <div class="text-center"> <h2 class="text-info">Kidu Bilton</h2> 
-                                                    <h5 class="text-info1">02154</h5>
-                                                    <img src="images/biltong.jpg" class="thumbnail">
-                                                    <h5>Time Left</h5>  <h2>24 : 12 : 10</h2>
-                                                    <h5 class="text-info">Highest Bid</h5>
-                                                    <h2 class="text-info">$33</h2>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="span4">
-                                        <div class="row-fluid">
-                                            <div class="span10">
-                                                <div class="text-center"> <h2 class="text-info">Kidu Bilton</h2> 
-                                                    <h5 class="text-info1">02154</h5>
-                                                    <img src="images/biltong.jpg" class="thumbnail">
-                                                    <h5>Time Left</h5>  <h2>24 : 12 : 10</h2>
-                                                    <h5 class="text-info">Highest Bid</h5>
-                                                    <h2 class="text-info">$33</h2>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>-->
+               
             </div>
             <div class="clearfix"></div>
         </div>
-        <!--ribbon--
-        <div class="ribbon">
-            <div class="container">
-                <h1>"Waar jou skat is, daar sal jou hart ook wees" (Matt.6:21)</h1>
-            </div>
-        </div>
-        <!--/ribbon-->
+        
 
         <!--footer-->
-        
-            <?php include('footer.php'); ?>
+
+        <?php include('footer.php'); ?>
         <!--/footer-->
 
-        <!--Feet--
-
-        <div class="feet">
-            <div class="container">
-                <p class="copy">Company Name Goes Here.. Copyright 2013. All Rights Researved. </p>
-
-            </div>
-        </div>
-
-        <!--/Feet-->
-
-
+        
         <script type="text/javascript">
             $(function()
             {
@@ -356,6 +311,7 @@ $townCombo.='</select>';
                         }
                     }); 
                 }); 
+                $("#auction_items h2[title]").tooltips();
             });
         </script>
         <script src="js/bootstrap.min.js"></script>                 
